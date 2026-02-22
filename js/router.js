af0c562b12a42f0aebe524dda17248e5be95efbf
gabrielsantos2033 <109436007+gabrielsantos2033@users.noreply.github.com>
2026-02-22 18:02:04 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,14 +1,30 @@
-import {renderHome} from "./feeds.js"
-import {renderDiscover} from "./feeds.js"
-import {renderSearch} from "./search.js"
-import {renderProfile} from "./profile.js"
+import { renderHome } from "./feeds.js"
+import { renderSearch } from "./search.js"
+import { renderProfile } from "./profile.js"
 
-function go(route,param=null){
-  if(route==="home") renderHome()
-  if(route==="discover") renderDiscover()
-  if(route==="search") renderSearch()
-  if(route==="profile") renderProfile(param)
-  if(route==="notifications") document.getElementById("app").innerHTML="<div class='card'>Em breve</div>"
+function go(route, param = null) {
+
+  if (route === "home") {
+    renderHome()
+  }
+
+  else if (route === "search") {
+    renderSearch()
+  }
+
+  else if (route === "profile") {
+    renderProfile(param)
+  }
+
+  else if (route === "discover") {
+    document.getElementById("app").innerHTML =
+      "<div class='card'>Discover em breve</div>"
+  }
+
+  else if (route === "notifications") {
+    document.getElementById("app").innerHTML =
+      "<div class='card'>Notificações em breve</div>"
+  }
 }
 
-export const router={go}
+export const router = { go }
