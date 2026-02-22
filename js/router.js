ef9e9f8125943e7ec15f0a0126d3ca3a94833cbd
gabrielsantos2033 <109436007+gabrielsantos2033@users.noreply.github.com>
2026-02-22 16:09:52 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,8 +1,14 @@
-export function router() {
-  const hash = window.location.hash.replace("#/", "") || "login";
-  return hash;
+import {renderHome} from "./feeds.js"
+import {renderDiscover} from "./feeds.js"
+import {renderSearch} from "./search.js"
+import {renderProfile} from "./profile.js"
+
+function go(route,param=null){
+  if(route==="home") renderHome()
+  if(route==="discover") renderDiscover()
+  if(route==="search") renderSearch()
+  if(route==="profile") renderProfile(param)
+  if(route==="notifications") document.getElementById("app").innerHTML="<div class='card'>Em breve</div>"
 }
 
-export function navigate(route) {
-  window.location.hash = route;
-}
+export const router={go}
