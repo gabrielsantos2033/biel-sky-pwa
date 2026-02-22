339f915c8fb976766098e697e61ba7d65c9dffe0
gabrielsantos2033 <109436007+gabrielsantos2033@users.noreply.github.com>
2026-02-22 17:04:23 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,11 +1,16 @@
-export let agent
+import { BskyAgent } from "https://esm.sh/@atproto/api"
+
+let agent
 
 export async function login(handle,password){
-  agent = new window.atproto.BskyAgent({
-    service: 'https://bsky.social'
+  agent = new BskyAgent({
+    service: "https://bsky.social"
   })
 
-  await agent.login({identifier:handle,password})
+  await agent.login({
+    identifier: handle,
+    password: password
+  })
 }
 
 export function getAgent(){
